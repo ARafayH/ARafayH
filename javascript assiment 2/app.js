e9cb9a0c3afa4d77d508c8e55745961884f6b675
ABDUL RAFAY <98506581+ARafayH@users.noreply.github.com>
2022-04-04 23:59:26 +0500

    Add files via upload

---
+++ javascript assiment 2/app.js
@@ -0,0 +1,175 @@
+// chapter 1 //
+
+// ARRAYS AND LOOP //
+
+// question 1 //
+// answer //
+// var arr =[
+    // [  ]
+    // [  ]
+// ];
+
+
+// question 2 //
+// answer //
+// var arr =[
+    // [0, 1, 2, 3 ]
+    // [1, 0, 1, 2 ]
+    // [2, 1, 0, 1 ]
+// ]
+
+
+// question 3 //
+// answer //
+// for (var i = 1; i >= 10; i++) {
+//  document.write(i);
+// }
+
+
+// question 4 //
+// answer //
+// var tableNumber = +prompt("enter table number");
+// var tableLength = +prompt("enter table Length");
+ 
+// if (tableNumber) {
+//   tableNumber = tableNumber;
+// } else {
+//   tableNumber = 5;
+// }
+
+// if (tableLength) {
+//   tableLength = tableLength;
+// } else {
+//   tableLength = 10;
+// }
+
+// for (var i = 1; i <= tableLength; i++) {
+//   document.write(
+//     tableNumber + " " + "x" + " " + i + " = " + tableNumber * i + "<br />"
+//   );
+// }
+
+
+// question 5 //
+// answer //
+// var fruitsArr = ["apple", "banana", "mango", "orange","strawberry"]
+
+
+// question 6 //
+// answer //
+
+// Counting: //
+// for (var i = 1; i >= 15; i++) {
+//  document.write(i);
+// }
+
+// Reverse counting: //
+// for (var i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// Even: //
+// for (var i = 2; i <= 10; i = i + 2) {
+//  document.write(i);
+// }
+
+// Odd: //
+// for (var i = 1; i <= 10; i = i + 2) {
+//  document.write(i);
+// }
+
+
+// question 7 //
+// answer //
+// var cookie = ["cake", "apple pie", "cookies", "chips", "patties"];
+// var isMatch = false;
+
+// for (var i = 0; i < cookie.length; i++) {
+//   if (usercookie === cookie[i]) {
+//     alert(" available");
+//     isMatch = true;
+//     break;
+//   }
+// }
+
+// if (isMatch === false) {
+//   alert("not available");
+// }
+
+
+// question 8 //
+// answer //
+// var array = [24 , 53, 78, 91, 12];
+// var largest= 91;
+
+// for (i=91; i<=largest;i++){
+//     if (array>largest) {
+//         var largest=array[i];
+//     }
+// }
+
+// document.write(largest);
+
+
+// question 9 //
+// answer //
+// var array = [24 , 53, 78, 91, 12];
+// var largest= 12;
+
+// for (i=12; i<=largest;i++){
+//     if (array>largest) {
+//         var largest=array[i];
+//     }
+// }
+
+// document.write(largest);
+
+
+// question 10 //
+// answer //
+// for (var i = 5; i <= 100; i++) {
+//   document.write("5 x " + i + " = " + 5 * i + "<br />");
+// }
+
+
+
+
+
+
+// chapter 2 //
+
+// ARRAYS //
+
+// question 1 //
+// answer //
+// var arr =[ ];
+
+
+// question 2 //
+// answer //
+// var arr =[ ];
+
+
+// question 3 //
+// answer //
+// var arrString =["A", "B", "C", "D", "E"] ;
+
+
+// question 4 //
+// answer //
+// var arrString =["1", "2", "3", "4", "5"] ;
+
+
+// question 5 //
+// answer //
+// var arrString =[true, false] ;
+
+
+// question 6 //
+// answer //
+// var arrString =[true, false, "1", "2", "3", "A", "B", "C", "4", "5", "D", "E"] ;
+
+
+// question 7 //
+// answer //
+// var qualification = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. phil.", "phD" ];
