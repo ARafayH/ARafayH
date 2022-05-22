0b841ba26d31522072bc852e4ad11a8e1edeebc8
ABDUL RAFAY <98506581+ARafayH@users.noreply.github.com>
2022-05-22 12:32:39 -0700

    1234

---
+++ js object/app.js
@@ -0,0 +1,35 @@
+// chapter 26
+
+// MATH METHODS 
+
+
+// question 1
+// asnwer
+/* Math.round10(55.55, -1);   // 55.6
+Math.round10(55.549, -1);  // 55.5
+Math.round10(55, 1);       // 60
+Math.round10(54.9, 1);     // 50
+// Floor
+Math.floor10(55.59, -1);   // 55.5
+Math.floor10(59, 1);       // 50
+// Ceil
+Math.ceil10(55.51, -1);    // 55.6
+Math.ceil10(51, 1);        // 60
+*/
+
+
+
+// question 2
+// asnwer
+/*
+Math.round10(-55.55, -1);  // -55.5
+Math.round10(-55.551, -1); // -55.6
+Math.round10(-55, 1);      // -50
+Math.round10(-55.1, 1);    // -60
+// Floor
+Math.floor10(-55.51, -1);  // -55.6
+Math.floor10(-51, 1);      // -60
+// Ceil
+Math.ceil10(-55.59, -1);   // -55.5
+Math.ceil10(-59, 1);       // -50
+*/
