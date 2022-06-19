d52ca9a129af8b6d4f9ed4d9be6f45e9d8f30b2d
ABDUL RAFAY <98506581+ARafayH@users.noreply.github.com>
2022-06-19 19:59:52 +0500

    Add files via upload

---
+++ class 1/app.js
@@ -0,0 +1,61 @@
+
+document.getElementById(mobiles)
+    let mobiles = {
+    iphone:  {
+     iphone14: {
+        model_name: "Iphone 14",
+        price: $700,
+        color: "black",
+        camera: "11",          
+},
+          iphone13: {
+        model_name: "Iphone 13",
+        price: $500,
+        color: "pink",
+        camera: "10",          
+},
+    },
+     
+
+          realme:  {
+     Realme5: {
+        model_name: "Realme 5",
+        price: $200,
+        color: "blue",
+        camera: "7",
+        ram: "4gb",
+        rom: "128gb",    
+},
+       
+     Realme6: {
+        model_name: "Realme 6",
+        price: $400,
+        color: "blue",
+        camera: "8",
+        ram: "8gb",
+        rom: "128gb" ,   
+},     
+
+},
+Motrola:  {
+    Motoe7: {
+       model_name: "Motoe7",
+       price: $100,
+       color: "green",
+       camera: "7",
+       ram: "4gb",
+       rom: "128gb",    
+},
+
+    Motoe6: {
+       model_name: "Motoe6",
+       price: $80,
+       color: "black",
+       camera: "5",
+       ram: "4gb",
+       rom: "64gb" ,   
+},     
+
+}
+}
+
